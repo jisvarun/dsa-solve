070a07994056527d2364b63c49be47c11f688204
jisvarun <jisbarunpandit@gmail.com>
2026-05-26 10:10:22 +0530

    Sum of Natural Numbers (n=3)

--- Kumar.java
+++
@@ -1,9 +0,0 @@
-public class Kumar{
-  public static void main(String[] args)
-  {
-    int[] arr = {10, 20, 30};
-    int n = arr.length;
-
-    System.out.println(n);
-  }
-}
---
+++ Main.java
@@ -0,0 +1,17 @@
+// Sum of Natural Numbers (n=3)
+// The sum of first 3 natural numbers is 1+2+3 = 6
+
+public class Main{
+
+  public static int sum(int n){
+    if(n == 1)
+      return 1;
+
+    return n + sum(n-1);
+  }
+  public static void main(String[] args)
+  {
+    int n = 7;
+    System.out.println(sum(n));
+  }
+}
